e78b51f200c79db97c8b967404852bc28b54b221
Neeraj319 <itskop520@gmail.com>
2022-07-24 11:07:45 +0545

    [removed] video title field

--- core/schema.py
+++ core/schema.py
@@ -5,7 +5,6 @@ import datetime
 
 class VideoPydanticModel(BaseModel):
     id: Optional[int] = None
-    title: Optional[str] = None
     url: Optional[str] = None
     created_at: Optional[datetime.datetime] = None
     status: Optional[str] = None
