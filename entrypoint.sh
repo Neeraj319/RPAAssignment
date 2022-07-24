74cb783b60acd99be96f259ddcc664bc8edacb27
Neeraj319 <itskop520@gmail.com>
2022-07-24 18:57:50 +0545

    [updated] env vars on dockerfile-compose

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 while !</dev/tcp/db/5432; do sleep 1; done;
 
+# Run only once
+alembic upgrade head
 uvicorn main:app --reload --host 0.0.0.0
--- worker/entrypoint.sh
+++ worker/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+while !</dev/tcp/db/5432; do sleep 1; done;
+
 dramatiq worker_init --watch .
